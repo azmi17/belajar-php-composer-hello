75cf1c0a6b13d136291e02bd5d10641ccae12e6f
Azmi Farhan <azmifarhan80@gmail.com>
2021-05-23 16:42:18 +0700

    contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace pzn\Belajar;
+
+class Customer {
+
+    
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name):string {
+        return "Hello $name, My Name is $this->name";
+    }
+
+
+
+
+    
+}
